4aac8febabfcd933f7e65bb1d37ebfd95ba4f5fd
Jatin Kaushik <jkprogrammer25@gmail.com>
2025-05-09 16:40:20 +0530

    add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,37 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "SoftSell - Turn Unused Software Licenses into Cash",
   description: "SoftSell helps businesses monetize unused software licenses with a simple, secure process and competitive valuations.",
+  keywords: [
+    "Soft shell",
+    "marketing",
+    "software licenses",
+    "license"
+  ],
+  authors: [{ name: "Jatin Kaushik" }],
+  creator: "Jatin Kaushik",
+  metadataBase: new URL("https://soft-shell-omega.vercel.app"),
+  openGraph: {
+    title: "SoftSell - Turn Unused Software Licenses into Cash",
+    description:
+      "SoftSell helps businesses monetize unused software licenses with a simple, secure process and competitive valuations.",
+    url: "https://soft-shell-omega.vercel.app",
+    siteName: "Soft shell",
+    images: [
+      {
+        url: "https://soft-shell-omega.vercel.app/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Soft shell logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SoftSell - Turn Unused Software Licenses into Cash",
+    description: "SoftSell helps businesses monetize unused software licenses with a simple, secure process and competitive valuations.",
+    images: ["https://soft-shell-omega.vercel.app/logo.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
